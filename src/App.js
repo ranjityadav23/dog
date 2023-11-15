1e6a62d4b8dc208fdd9e0350eeda6988abf2b950
randi <randi80@gmail.com>
2023-11-15 11:08:11 +0000

    ghcdvf

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <html>
+    <head>
+    </head>
+      <body>
+        <p>this is my class</p>
+      </body>
+  </html>
+  ) 
+};
+
+export default App;
